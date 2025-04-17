89466c0ccbca79cdc80b378440418deea107742b
Matheus <matheusteodorogarcia@gmail.com>
2025-04-16 22:28:44 -0300

    feat: add theme config and theme type

---
+++ source/web/src/@types/theme.ts
@@ -0,0 +1,29 @@
+export type Theme = {
+  colors: {
+    background: string;
+    foreground: string;
+
+    primary100: string;
+    primary300: string;
+    primary500: string;
+    primary700: string;
+    primary900: string;
+
+    gray50: string;
+    gray100: string;
+    gray200: string;
+    gray300: string;
+    gray400: string;
+    gray500: string;
+    gray600: string;
+    gray700: string;
+
+    danger300: string;
+    danger500: string;
+    danger700: string;
+
+    safe300: string;
+    safe500: string;
+    safe700: string;
+  };
+};
---
+++ source/web/src/theme/index.tsx
@@ -0,0 +1,31 @@
+import { Theme } from "@/@types/theme";
+
+export const theme: Theme = {
+  colors: {
+    background: "#ffffff",
+    foreground: "#111111",
+
+    primary100: "#4a67df",
+    primary300: "#4a67df",
+    primary500: "#172c82",
+    primary700: "#0b1641",
+    primary900: "#060c23",
+
+    gray50: "#ffffff",
+    gray100: "#f6f6f6",
+    gray200: "#e7e7e7",
+    gray300: "#808080",
+    gray400: "#6a6a6a",
+    gray500: "#222222",
+    gray600: "#111111",
+    gray700: "#000000",
+
+    danger300: "#d52a3e",
+    danger500: "#bf2234",
+    danger700: "#ad1f2f",
+
+    safe300: "#28a847",
+    safe500: "#1f8438",
+    safe700: "#17632a",
+  },
+};
