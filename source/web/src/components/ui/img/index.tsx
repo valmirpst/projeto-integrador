01a5e164097cbd2fbe62ab5601e9b34363944bd6
Matheus <matheusteodorogarcia@gmail.com>
2025-04-18 18:38:42 -0300

    refactor(img): improve Img component

--- source/web/src/components/ui/img/index.tsx
+++ source/web/src/components/ui/img/index.tsx
@@ -1,17 +1,22 @@
 import Image, { ImageProps } from "next/image";
 import styles from "./img.module.css";
+import { cnModules } from "@/lib/cnModules";
 
-type Props = {} & ImageProps;
+type Props = ImageProps;
 
 export default function Img(props: Props) {
-  const { src, width, height, alt } = props;
+  const { src, width, height, alt, className, ...args } = props;
+
+  const classes = cnModules(styles.img, className);
+
   return (
     <Image
-      className={styles.img}
+      className={classes}
       src={src}
       width={width}
       height={height}
       alt={alt}
+      {...args}
     />
   );
 }
