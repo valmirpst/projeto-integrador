4dc3e82e571523e0149f01c638784b2c17655551
Valmir Paiva Stachin <valmir@unimake.com.br>
2025-04-24 09:56:06 -0300

    feat (api): Add core functions

---
+++ source/api/src/core/database.ts
@@ -0,0 +1,14 @@
+import { Pool, QueryConfig } from "pg";
+import { env } from "../env";
+
+const pool = new Pool({
+  connectionString: env.POSTGRES_URL,
+});
+
+pool.on("connect", () => {
+  console.log("Base de Dados conectado com sucesso!");
+});
+
+export const db = {
+  query: (text: string | QueryConfig<any>, params?: any) => pool.query(text, params),
+};
