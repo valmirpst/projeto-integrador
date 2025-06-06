79b0221b8bcdf301ddf4401fcc63e8483c575565
Matheus <matheusteodorogarcia@gmail.com>
2025-06-05 21:05:01 -0300

    refactor: adjustments to the structure of the book registration form

--- source/web/src/components/register-livro-modal/index.tsx
+++ source/web/src/components/register-livro-modal/index.tsx
@@ -42,7 +42,6 @@ export default function RegisterLivroModal({ open, onOpenChange }: PropsType) {
   };
 
   const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    console.log("teste");
     // const file = e.target.files?.[0];
     const file = "testeeeeFileee";
     if (file) {
@@ -52,7 +51,6 @@ export default function RegisterLivroModal({ open, onOpenChange }: PropsType) {
   };
 
   const handleSubmitChange = async () => {
-    console.log("form", form);
     const isValid = Object.values(form).every(
       (value) => value !== "" && value !== null
     );
@@ -86,92 +84,94 @@ export default function RegisterLivroModal({ open, onOpenChange }: PropsType) {
 
   return (
     <Dialog.Root open={open} onOpenChange={onOpenChange}>
-      <Dialog.Trigger asChild>
-        <Text className={styles.addLivro}>Adicionar Livro</Text>
-      </Dialog.Trigger>
-
-      <Dialog.Content width={800} className={styles.modalContent}>
-        <Box className={styles.modalInputs}>
-          <Dialog.Title className={styles.modalTitle}>Bem-vindo</Dialog.Title>
-          <Dialog.Close />
-          <Input
-            id="titulo"
-            label="Título"
-            value={form.titulo}
-            onChange={(e) => handleChange(e, "titulo")}
-          />
-          <Box className={styles.row}>
+      <Dialog.Content width={800} className={styles.modalContainer}>
+        <Box className={styles.modalContent}>
+          <Box className={styles.modalInputs}>
+            <Dialog.Title className={styles.modalTitle}>
+              Registrar Livro
+            </Dialog.Title>
+            <Dialog.Close />
             <Input
-              id="edicao"
-              label="Edição"
-              value={form.edicao}
-              onChange={(e) => handleChange(e, "edicao")}
+              id="titulo"
+              label="Título"
+              value={form.titulo}
+              onChange={(e) => handleChange(e, "titulo")}
             />
+            <Box className={styles.row}>
+              <Input
+                id="edicao"
+                label="Edição"
+                value={form.edicao}
+                onChange={(e) => handleChange(e, "edicao")}
+              />
+              <Input
+                id="genero"
+                label="Gênero"
+                value={form.genero}
+                onChange={(e) => handleChange(e, "genero")}
+              />
+            </Box>
             <Input
-              id="genero"
-              label="Gênero"
-              value={form.genero}
-              onChange={(e) => handleChange(e, "genero")}
+              id="editora"
+              label="Editora"
+              value={form.editora}
+              onChange={(e) => handleChange(e, "editora")}
             />
+            <Box>
+              <Input
+                id="autores"
+                label="Autores"
+                onBlur={handleAutor}
+                value={autor}
+                onChange={(e) => setAutor(e.target.value)}
+              />
+              <Text>{form.autores.join(" - ")}</Text>
+            </Box>
+            <Box>
+              <label htmlFor="descricao" className={styles.label}>
+                Descrição
+              </label>
+              <textarea
+                id="descricao"
+                className={styles.textarea}
+                value={form.descricao}
+                onChange={(e) => handleChange(e, "descricao")}
+              />
+            </Box>
+            <Box className={styles.footer}>
+              <label htmlFor="quantidade" className={styles.label}>
+                Quantidade
+              </label>
+              <input
+                id="quantidade"
+                type="number"
+                min={1}
+                className={styles.inputSmall}
+                value={form.qtd_disponivel}
+                onChange={(e) =>
+                  setForm((prev) => ({
+                    ...prev,
+                    quantidade: parseInt(e.target.value),
+                  }))
+                }
+              />
+            </Box>
           </Box>
-          <Input
-            id="editora"
-            label="Editora"
-            value={form.editora}
-            onChange={(e) => handleChange(e, "editora")}
-          />
-          <Input
-            id="autores"
-            label="Autores"
-            onBlur={handleAutor}
-            value={autor}
-            onChange={(e) => setAutor(e.target.value)}
-          />
-          <Text>{form.autores.join(" ")}</Text>
-
-          <label htmlFor="descricao" className={styles.label}>
-            Descrição
-          </label>
-          <textarea
-            id="descricao"
-            className={styles.textarea}
-            value={form.descricao}
-            onChange={(e) => handleChange(e, "descricao")}
-          />
-          <Box className={styles.footer}>
-            <label htmlFor="quantidade" className={styles.label}>
-              Quantidade
-            </label>
+          <Box className={styles.modalImageContainer}>
+            <Img
+              className={styles.modalImage}
+              src={previewUrl || "/image-example.svg"}
+              alt="Pré-visualização da imagem"
+              width={220}
+              height={300}
+            />
             <input
-              id="quantidade"
-              type="number"
-              min={1}
-              className={styles.inputSmall}
-              value={form.qtd_disponivel}
-              onChange={(e) =>
-                setForm((prev) => ({
-                  ...prev,
-                  quantidade: parseInt(e.target.value),
-                }))
-              }
+              id="img"
+              type="file"
+              accept="image/*"
+              onChange={handleImageChange}
             />
           </Box>
-        </Box>
-
-        <Box className={styles.modalImageContainer}>
-          <Img
-            className={styles.modalImage}
-            src={previewUrl || "/image-example.svg"}
-            alt="Pré-visualização da imagem"
-            width={220}
-            height={300}
-          />
-          <input
-            id="img"
-            type="file"
-            accept="image/*"
-            onChange={handleImageChange}
-          />
         </Box>
 
         <Dialog.Actions className={styles.actions}>
